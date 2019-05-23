0353e88fd78da92d6e53f221e4eba7eecf9e2419
rpiwko <radoslaw.piwko@gmail.com>
2019-05-23 15:41:48 +0200

    Displaying the actual data

--- js/main.js
+++ js/main.js
@@ -1,27 +1,38 @@
-function drawChart() {
-    var data = {
-        // A labels array that can contain any sort of values
-        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-        // Our series array that contains series objects or in this case series data arrays
-        series: [
-            [5, 2, 4, 2, 0]
-        ]
-        };
+
+function drawSingleChart(tagId, resultsSet) {
+
+    console.log(resultsSet);
+    var charXs = [];
+    var charYs = [];
+    for (var property in resultsSet) {        
+        charXs.push(property)
+        charYs.push(resultsSet[property] / 1000)
+    }
+    console.log(charXs);
+    console.log(charYs);
+
+    charData = {
+        labels: charXs.slice(0, 8),
+        series: [ charYs.slice(0, 8) ]
+    };
 
     var options = {
-        width: 500,
-        height: 300
-        };
+        width: 800,
+        height: 200
+    };
 
-    // Create a new line chart object where as first parameter we pass in a selector
-    // that is resolving to our chart container element. The Second parameter
-    // is the actual data object.
-    new Chartist.Line("#avg-renting-price", data, options);
-    new Chartist.Line("#avg-selling-price", data, options);    
-    new Chartist.Line("#avg-square-meter-price", data, options);
-    console.log("Done!");
+    new Chartist.Line(tagId, charData, options);
+}
+
+function drawCharts(dbExtract) {
+
+    drawSingleChart("#avg-apartment-renting-price", dbExtract.results[0].results);
+    drawSingleChart("#avg-apartment-selling-price", dbExtract.results[1].results);
+    drawSingleChart("#avg-apartment-square-meter-price", dbExtract.results[2].results);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    drawChart();
+    $.getJSON("../../data/results.json", function(dbExtract) {        
+        drawCharts(dbExtract);
+    });    
 })
